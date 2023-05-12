4af837e769d70eabdf0755ee97990c462b45b2d0
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-05-12 07:38:46 -0700

    Refactor form date validation

--- script.js
+++ script.js
@@ -6,62 +6,58 @@ submitButton.addEventListener("click", (e) => {
   // ensure there are no empty fields
   const form = document.querySelector(".ac__form");
   const data = new FormData(form);
-  const inputs = document.querySelectorAll(".ac__field-input");
-  const [dayField, monthField, yearField] = inputs;
-  console.log(yearField.value);
 
+  // verify that all fields are filled out
   let index = 0,
     errorFound = false;
-  for (const [_, value] of data) {
+  for (const [name, value] of data) {
     if (value === "") {
-      const errorMessage = generateErrorMessage("This field is required");
-      inputs[index].after(errorMessage);
+      generateErrorMessage(name, "This field is required");
       errorFound = true;
     }
     index++;
   }
 
-  const day = data.get("day");
-  const month = data.get("month");
-  const year = data.get("year");
-  const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (errorFound) {
+    addErrorStyling();
+    return;
+  }
+
+  const day = parseInt(data.get("day"));
+  const month = parseInt(data.get("month"));
+  const year = parseInt(data.get("year"));
 
   // check if month is valid
   if (month < 1 || month > 12) {
-    const errorMessage = generateErrorMessage("Must be a valid month");
-    monthField.after(errorMessage);
-    monthField.classList.add("ac__field-input--error");
+    generateErrorMessage("month", "Must be a valid month");
     errorFound = true;
   }
   // check if day is valid
   if (day < 1 || day > 31) {
-    const errorMessage = generateErrorMessage("Must be a valid day");
-    dayField.after(errorMessage);
-    dayField.classList.add("ac__field-input--error");
+    generateErrorMessage("day", "Must be a valid day");
     errorFound = true;
-  }
-  // check if leap year
-  if (month === 2) {
+    // check if valid month and day
+  } else if (month >= 1 && month <= 12) {
+    // check if leap year
     if (year % 4 === 0 && year % 100 !== 0) {
-      if (day > 29) {
-        const errorMessage = generateErrorMessage("Must be a valid day");
-        dayField.after(errorMessage);
-        dayField.classList.add("ac__field-input--error");
+      const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (day > daysInMonth[month - 1] || day < 1) {
+        generateErrorMessage("day", "Must be a valid day");
+        errorFound = true;
+      }
+    } else {
+      const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      if (day > daysInMonth[month - 1] || day < 1) {
+        generateErrorMessage("day", "Must be a valid day");
         errorFound = true;
       }
     }
-  } else if (day > daysInMonth[month - 1]) {
-    const errorMessage = generateErrorMessage("Must be a valid day");
-    dayField.after(errorMessage);
-    dayField.classList.add("ac__field-input--error");
-    errorFound = true;
   }
 
   // check if year is valid
   if (year > new Date().getFullYear()) {
-    const errorMessage = generateErrorMessage("Must be in the past");
-    yearField.after(errorMessage);
-    yearField.classList.add("ac__field-input--error");
+    generateErrorMessage("year", "Must be in the past");
+    errorFound = true;
   }
 
   if (errorFound) {
@@ -70,11 +66,16 @@ submitButton.addEventListener("click", (e) => {
   }
 });
 
-function generateErrorMessage(message) {
+function generateErrorMessage(field, message) {
+  if (!["day", "month", "year"].includes(field)) {
+    throw new Error("Invalid field");
+  }
   const errorMessage = document.createElement("span");
   errorMessage.classList.add("ac__error-message");
   errorMessage.textContent = message;
-  return errorMessage;
+
+  const input = document.querySelector(`.ac__field-input[name=${field}]`);
+  input.after(errorMessage);
 }
 
 function removeErrorStyling() {
