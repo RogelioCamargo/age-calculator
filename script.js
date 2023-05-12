538059ceab4ea776bf038e2de4fe09fcbb491d71
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-05-12 08:28:14 -0700

    Refactor

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const submitButton = document.querySelector(".ac__submit-button");
 submitButton.addEventListener("click", (e) => {
   e.preventDefault();
-  // remove any existing error styling
+  // Remove any existing error styling
   removeErrorStyling();
-  // ensure there are no empty fields
+
+  // Ensure there are no empty fields
   const form = document.querySelector(".ac__form");
   const data = new FormData(form);
 
-  // verify that all fields are filled out
-  let index = 0,
-    errorFound = false;
+  // Verify that all fields are filled out
+  let errorFound = false;
+
+  // Iterate through form data entries
   for (const [name, value] of data) {
     if (value === "") {
       generateErrorMessage(name, "This field is required");
       errorFound = true;
     }
-    index++;
   }
 
   if (errorFound) {
@@ -27,34 +28,24 @@ submitButton.addEventListener("click", (e) => {
   const month = parseInt(data.get("month"));
   const year = parseInt(data.get("year"));
 
-  // check if month is valid
-  if (month < 1 || month > 12) {
-    generateErrorMessage("month", "Must be a valid month");
-    errorFound = true;
-  }
-  // check if day is valid
+  // Check if day is valid
   if (day < 1 || day > 31) {
     generateErrorMessage("day", "Must be a valid day");
     errorFound = true;
-    // check if valid month and day
   } else if (month >= 1 && month <= 12) {
-    // check if leap year
-    if (year % 4 === 0 && year % 100 !== 0) {
-      const daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      if (day > daysInMonth[month - 1] || day < 1) {
-        generateErrorMessage("day", "Must be a valid day");
-        errorFound = true;
-      }
-    } else {
-      const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-      if (day > daysInMonth[month - 1] || day < 1) {
-        generateErrorMessage("day", "Must be a valid day");
-        errorFound = true;
-      }
+    // Check if valid month and day
+    const isLeapYear = year % 4 === 0 && year % 100 !== 0;
+    const daysInMonth = isLeapYear
+      ? [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+      : [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    if (day > daysInMonth[month - 1] || day < 1) {
+      generateErrorMessage("day", "Must be a valid day");
+      errorFound = true;
     }
   }
 
-  // check if year is valid
+  // Check if year is valid
   if (year > new Date().getFullYear()) {
     generateErrorMessage("year", "Must be in the past");
     errorFound = true;
@@ -65,36 +56,36 @@ submitButton.addEventListener("click", (e) => {
     return;
   }
 
-	// calculate age in terms of years, months, and days from birthday
-	const today = new Date();
-	const birthday = new Date(year, month - 1, day);
-	// calculate raw differences
-	const age = today.getFullYear() - birthday.getFullYear();
-	const monthDifference = today.getMonth() - birthday.getMonth();
-	const dayDifference = today.getDate() - birthday.getDate();
-	let ageInYears = age;
-	let ageInMonths = monthDifference;
-	let ageInDays = dayDifference;
+  // calculate age in terms of years, months, and days from birthday
+  const today = new Date();
+  const birthday = new Date(year, month - 1, day);
+  // calculate raw differences
+  const age = today.getFullYear() - birthday.getFullYear();
+  const monthDifference = today.getMonth() - birthday.getMonth();
+  const dayDifference = today.getDate() - birthday.getDate();
+  let ageInYears = age;
+  let ageInMonths = monthDifference;
+  let ageInDays = dayDifference;
 
-	// adject years and months if birthday hasn't occurred this month
-	if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
-		ageInYears--;
-		ageInMonths = 12 + monthDifference;
-	}
-	if (dayDifference < 0) {
+  // adject years and months if birthday hasn't occurred this month
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    ageInYears--;
+    ageInMonths = 12 + monthDifference;
+  }
+  if (dayDifference < 0) {
     ageInMonths--;
-		// calculate the number of days between today and last month
+    // calculate the number of days between today and last month
     const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 1);
     ageInDays += (today - lastMonth) / (1000 * 60 * 60 * 24);
   }
 
-	// display age
-	const ageDisplay = document.querySelector(".ac__result-years");
-	ageDisplay.textContent = ageInYears;
-	const ageDisplayMonths = document.querySelector(".ac__result-months");
-	ageDisplayMonths.textContent = ageInMonths;
-	const ageDisplayDays = document.querySelector(".ac__result-days");
-	ageDisplayDays.textContent = ageInDays;
+  // display age
+  const ageDisplay = document.querySelector(".ac__result-years");
+  ageDisplay.textContent = ageInYears;
+  const ageDisplayMonths = document.querySelector(".ac__result-months");
+  ageDisplayMonths.textContent = ageInMonths;
+  const ageDisplayDays = document.querySelector(".ac__result-days");
+  ageDisplayDays.textContent = ageInDays;
 });
 
 function generateErrorMessage(field, message) {
